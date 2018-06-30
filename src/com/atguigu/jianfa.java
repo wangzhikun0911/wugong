cc091cc8981f60b08e623dbe070535af17d0f96b
chen <794296713@qq.com>
2018-06-30 15:06:44 +0800

    fgvb

--- src/com/atguigu/jianfa.java
+++ src/com/atguigu/jianfa.java
@@ -5,5 +5,8 @@ public class jianfa {
 		System.out.println("1111111111111111");
 		System.out.println("2222222222222222");
 		System.out.println("3333333333333333");
+		System.out.println("3333333333333333");System.out.println("3333333333333333");System.out.println("3333333333333333");
+
+
 	}
 }
